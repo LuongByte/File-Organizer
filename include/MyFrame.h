bcd0b6dd9ba17eaf03f26b53566744b5d900e3c9
Vinh Nguyen <luong_nguyen@sfu.ca>
2025-10-07 20:21:12 -0700

    Installed wxWidgets and set up in VSCode

---
+++ include/MyFrame.h
@@ -0,0 +1,18 @@
+#include <wx/wx.h>
+#include <wx/wxprec.h>
+
+class MyFrame : public wxFrame
+{
+public:
+    MyFrame();
+ 
+private:
+    void OnHello(wxCommandEvent& event);
+    void OnExit(wxCommandEvent& event);
+    void OnAbout(wxCommandEvent& event);
+};
+
+enum
+{
+    ID_Hello = 1
+};
---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <wx/wx.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <filesystem>
+#include <MyFrame.h>
+ 
+class MyApp : public wxApp
+{
+public:
+    bool OnInit() override;
+};
+ 
+wxIMPLEMENT_APP(MyApp);
+ 
+ 
+bool MyApp::OnInit()
+{   
+  //  SetAppear
+    std::string input_path;
+    std::cout << "Enter a file path: ";
+    
+    std::cin >> input_path;
+    std::filesystem::path dirPath = input_path;
+    
+    std::filesystem::create_directories(dirPath);
+    MyFrame *frame = new MyFrame();
+    //frame->SetBackgroundColour(wxColour(30, 30, 30));
+    frame->Show(true);
+    
+    return true;
+}
